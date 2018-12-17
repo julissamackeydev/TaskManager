6b616afa41f8fcb494ea4417e4f715714c5c8d2f
julissamackeydev <julissamackey@Julissas-MacBook-Air.local>
2018-12-16 22:40:06 -0500

    Reactive form sends new task to server

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { Routes, RouterModule } from '@angular/router';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskComponent } from './task/task.component';
@@ -20,6 +20,7 @@ const appRoutes: Routes = [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(
       appRoutes
     )
--- client/src/app/task/task.component.ts
+++ client/src/app/task/task.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from '../services/task.service';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
@@ -8,15 +8,29 @@ import { TaskService } from '../services/task.service';
 })
 export class TaskComponent implements OnInit {
 
+  tasks:{};
+  form: FormGroup;
+  // name: string = "";
+  // priority:string = "";
+  // description:string = "";
+
   constructor(
-    private taskService: TaskService
-  ) { }
-  tasks = [];
+    private taskService: TaskService,
+    private formBuilder: FormBuilder
+  ) { 
+    this.form = formBuilder.group({
+      'name': [null, Validators.required],
+      'description': [null],
+      'priority' :[null, Validators.required]
+    })
+  }
+
 
   ngOnInit() {
     this.taskService.getTasks().subscribe(response=>{
-      this.tasks.push(response)
-      console.log(this.tasks);
+      console.log(typeof(response))
+      console.log(response)
+      this.tasks = response;
     })
     // this.taskService.addTask({
     //   name: 'post request',
@@ -32,6 +46,16 @@ export class TaskComponent implements OnInit {
     // this.taskService.deleteTask('5c0f402fa4af123eeaa29c61').subscribe(response=>{
     //   console.log(response);
     // })
+  }
+  addTask(form){
+    
+    let task = {
+      name : form.name,
+      description : form.description,
+      priority : form.priority
+    };
+    this.taskService.addTask(task)
+
   }
 
 }
